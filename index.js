d6454ac2369bd7e6be0947e269442cbcd5c1fe15
George Raptis <georapbox@gmail.com>
2017-04-13 10:34:43 +0300

    Add API

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  push: require('./lib/immutable-push'),
+  pop: require('./lib/immutable-pop'),
+  shift: require('./lib/immutable-shift'),
+  unshift: require('./lib/immutable-unshift'),
+  splice: require('./lib/immutable-splice'),
+  reverse: require('./lib/immutable-reverse'),
+  delete: require('./lib/immutable-delete')
+};
