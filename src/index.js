d27f57f73e247753bc244b9c411b49f956496a2d
George Raptis <georapbox@gmail.com>
2017-04-13 16:03:31 +0300

    Export modules with default names to avoid confusion

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import {default as push} from './immutable-push';
-import {default as pop} from './immutable-pop';
-import {default as shift} from './immutable-shift';
-import {default as unshift} from './immutable-unshift';
-import {default as splice} from './immutable-splice';
-import {default as reverse} from './immutable-reverse';
-import {default as del} from './immutable-delete';
-export default {push, pop, shift, unshift, splice, reverse, del};
+import immutablePush from './immutable-push';
+import immutablePop from './immutable-pop';
+import immutableShift from './immutable-shift';
+import immutableUnshift from './immutable-unshift';
+import immutableSplice from './immutable-splice';
+import immutableReverse from './immutable-reverse';
+import immutableDelete from './immutable-delete';
+
+export default {
+  immutablePush,
+  immutablePop,
+  immutableShift,
+  immutableUnshift,
+  immutableSplice,
+  immutableReverse,
+  immutableDelete
+};
