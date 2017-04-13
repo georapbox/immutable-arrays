e5e174110e7df8895b580b5a7cb6bfa86bb626ca
George Raptis <georapbox@gmail.com>
2017-04-13 15:29:46 +0300

    Provide default values
    Provide default values to comply as much as possible to original
    Array.prototype.splice method

--- src/immutable-splice.js
+++ src/immutable-splice.js
@@ -2,11 +2,26 @@
  * Removes existing elements and/or adds new elements to an array.
  *
  * @param {Array} array The original array.
- * @param {Number} start Zero based index at which to start changing the array.
- * @param {Number} [deleteCount] An integer indicating the number of old array elements to remove.
- * @param {*} elementN The elements to add to the array, beginning at the start index. If you don't specify any elements, splice() will only remove elements from the array.
+ * @param {Number} [start = array.length]
+ *        Zero based index at which to start changing the array.
+ *        If greater than the length of the array, actual starting index will be set to the length of the array.
+ * @param {Number} [deleteCount = array.length - start]
+ *        An integer indicating the number of old array elements to remove.
+ *        If `deleteCount` is 0, no elements are removed.
+ *        If `deleteCount` is lower than 0, `deleteCount` will be equal to 0.
+ *        If `deleteCount` is greater than the number of elements left in the array starting at
+ *        `start`, then all of the elements through the end of the array will be deleted.
+ *        If `deleteCount` is omitted, `deleteCount` will be equal to (`array.length - start`),
+ *        i.e., all of the elements beginning with `start` index on through the end of the array will be deleted.
+ * @param {*} [elementN]
+ *        The elements to add to the array, beginning at the start index.
+ *        If you don't specify any elements, will only remove elements from the array.
  * @return {Array} The result array.
  */
-export default function immutableSplice(array, start, deleteCount, ...elementN) {
-  return [...array.slice(0, start), ...elementN, ...array.slice(start + deleteCount)];
+export default function immutableSplice(array, start = array.length, deleteCount = array.length - start, ...elementN) {
+  return [
+    ...array.slice(0, start),
+    ...elementN,
+    ...array.slice(start + (deleteCount < 0 ? 0 : deleteCount))
+  ];
 }
