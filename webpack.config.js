013c6faa6b1901584890920e8a9e85b588f7071d
George Raptis <georapbox@gmail.com>
2018-10-31 16:42:31 +0200

    update all devDependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,30 @@
-'use strict'; // eslint-disable-line
-
 const webpack = require('webpack');
-const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-const env = require('yargs').argv.env;
+const mode = require('yargs').argv.mode;
+const libraryTarget = require('yargs').argv['output-library-target'];
 const pkg = require('./package.json');
 
 const libraryName = 'immutableArrays';
 
-const banner = `${pkg.name} - ${pkg.description}\n
+const banner = `${pkg.name}
+${pkg.description}\n
 @version v${pkg.version}
 @author ${pkg.author}
 @homepage ${pkg.homepage}
 @repository ${pkg.repository.url}`;
 
-let outputFile;
-
 const plugins = [
   new webpack.BannerPlugin(banner)
 ];
 
-if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({minimize: true, sourceMap: true}));
-  outputFile = `${libraryName}.min.js`;
-} else {
-  outputFile = `${libraryName}.js`;
-}
-
-const config = {
-  entry: `${__dirname}/src/index.js`,
+module.exports = {
+  entry: `${__dirname}/index.js`,
   devtool: 'source-map',
   output: {
-    path: `${__dirname}/lib`,
-    filename: outputFile,
+    path: `${__dirname}/${libraryTarget === 'umd' ? 'dist/umd' : 'dist/commonjs'}`,
+    filename: mode === 'development' ? `${libraryName}.js` : `${libraryName}.min.js`,
     library: libraryName,
-    libraryTarget: 'umd',
+    libraryTarget: libraryTarget || 'umd',
+    globalObject: '(typeof self !== \'undefined\' ? self : this)', // TODO Hack (for Webpack 4+) to enable create UMD build which can be required by Node without throwing error for window being undefined (https://github.com/webpack/webpack/issues/6522)
     umdNamedDefine: true
   },
   module: {
@@ -52,5 +43,3 @@ const config = {
   },
   plugins: plugins
 };
-
-module.exports = config;
